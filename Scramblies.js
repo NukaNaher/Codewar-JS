c48221037ab3273e1d52b22b883addd193490002
NukaNaher <117821124+NukaNaher@users.noreply.github.com>
2022-11-27 19:36:54 +0300

    Initial commit

---
+++ Scramblies.js
@@ -0,0 +1,13 @@
+const scramble = (str1, str2) => {
+    const occurences = str1.split('').reduce((result, item) => {
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1;
+      }
+      return result;
+    }, {});
+    return str2.split('').every(character => --occurences[character] >= 0);
+  };
+  
+  global.scramble = scramble;
